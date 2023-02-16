be56d4da93ed386e2581646c6d307f9f9fc195c5
Beksultan Esenkulov <esenkulb@bis.berlin-chemie.com>
2023-02-16 10:54:02 +0600

    create task added

--- main/views.py
+++ main/views.py
@@ -1,5 +1,6 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .models import Task
+from .forms import TaskForm
 
 
 def index(request):
@@ -12,5 +13,18 @@ def about(request):
 
 
 def create(request):
-    return render(request, 'main/create.html')
+    error = ''
+    if request.method == 'POST':
+        form = TaskForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('home')
+        else:
+            error = 'Incorrect input'
+    form = TaskForm()
+    context = {
+        'form': form,
+        'error': error
+    }
+    return render(request, 'main/create.html', context)
 
